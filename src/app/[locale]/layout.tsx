cb85d6d4b65de3027e4cb0fef01f7b248d57f82d
protam113 <hoangpm2003.strix@gmail.com>
2026-05-16 16:48:44 +0700

    fix

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -47,6 +47,13 @@ export default async function LocaleLayout({
           gtag('js', new Date());
           gtag('config', 'G-QLE41CQ8TP');
         `}</Script>
+        <Script id="gtm-script" strategy="afterInteractive">{`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-W9HJ7NXD');
+        `}</Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
@@ -65,6 +72,14 @@ export default async function LocaleLayout({
         <JsonLd />
       </head>
       <body className={`antialiased ${inter.variable}`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W9HJ7NXD"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <LoadingProvider>
           <MobileProvider>
             <ReactQueryProvider>
