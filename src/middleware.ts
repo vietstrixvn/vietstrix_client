0a189ce6badf72be6eeac84af317cc0285293e00
protam113 <hoangpm2003.strix@gmail.com>
2026-05-23 11:41:55 +0700

    fix: update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,23 @@
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
+import { NextRequest, NextResponse } from 'next/server';
 
-export default createMiddleware(routing);
+const intlMiddleware = createMiddleware(routing);
+
+export default function middleware(request: NextRequest) {
+  const userAgent = request.headers.get('user-agent') || '';
+
+  const isBot =
+    /facebookexternalhit|Twitterbot|Googlebot|bingbot|crawler|spider|bot/i.test(
+      userAgent
+    );
+
+  if (isBot) {
+    return NextResponse.next();
+  }
+
+  return intlMiddleware(request);
+}
 
 export const config = {
   matcher: [
